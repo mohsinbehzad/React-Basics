37d3a4dec8579c657f781fcc548891554b664072
“mohsinbehzad” <mohsinbehzad14@gmail.com>
2025-05-24 17:54:02 +0500

    Header added

---
+++ 01.First-Project/src/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <>
+            <h2>My Website</h2>
+            <ul>
+                <li> <a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </>
+    );
+}
+
+export default Header
