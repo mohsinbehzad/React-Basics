f0b8423018c2aa82e1cc97ca35d2774edbef6bb9
“mohsinbehzad” <mohsinbehzad14@gmail.com>
2025-05-24 18:08:31 +0500

    Footer added

---
+++ 01.First-Project/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <>
+            <hr />
+            <p>Copyright &copy; 2025 React - All right reserved </p>
+        </>
+    );
+}
+
+export default Footer
