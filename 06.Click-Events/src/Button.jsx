f8eed1fcc99175e567dc9a72726f04956cc86c9d
“mohsinbehzad” <mohsinbehzad14@gmail.com>
2025-05-25 17:19:21 +0500

    CSS and conditional onClick added

--- 06.Click-Events/src/Button.jsx
+++ 06.Click-Events/src/Button.jsx
@@ -1,8 +1,19 @@
 function Button() {
 
-    const handleClick = (()=> console.log("clicked"))
-    return(
-        <button onClick={handleClick}>Click me</button>
+    // const handleClick = (()=> console.log("clicked"))
+    let count = 0;
+
+    const handleClick = (name) => {
+        if (count < 3) {
+            count++;
+            console.log(`${name} clicked me ${count} times`);
+        }
+        else {
+            console.log(`${name} don't click me`);
+        }
+    }
+    return (
+        <button className="btn" onClick={() => handleClick('Mohsin')}>Click me</button>
     );
 }
 
