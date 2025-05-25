ee154f24cc9636188d607e749cf63c785ac6f94a
“mohsinbehzad” <mohsinbehzad14@gmail.com>
2025-05-25 19:59:58 +0500

    ReactHook and useState() defintion added

--- 07.UseState-hook/src/App.jsx
+++ 07.UseState-hook/src/App.jsx
@@ -1,3 +1,11 @@
+// React hook = Special function that allows functinal components
+//              To use React features without writing class Components
+//              (useState, useEffect, useContext, useReducer, useCallback etc)
+
+// useState() = A React hook that allows the creation of a stateful variable
+//              And a Setter function to update its value in the Virtual DOM
+//              [name, setName]
+
 import Counter from "./Counter"
 import MyComponent from "./MyComponent"
 
